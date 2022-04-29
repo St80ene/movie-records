6948f11dcf580158bdfc68adc41cf1b6dc555b9c
etiene <etienejames5@gmail.com>
2022-04-29 20:43:50 +0100

    added git ignore of yarn files

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the World of Movies!';
+    return 'Welcome to the World of Movies...Enjoy!';
   }
 }
